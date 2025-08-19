f62f024e9e5aba039ede6653124c060cb7b90ea5
Bhagya laxmi <Bhagya laxmi@Bhagya14>
2025-08-19 15:18:05 +0530

    datatypes

---
+++ tns/src/DatatypesDemo.java
@@ -0,0 +1,28 @@
+
+public class DatatypesDemo {
+
+	public static void main(String[] args) {
+		 // Primitive Data Types
+        byte myByte = 127;
+        short myShort = 32000;
+        int myInt = 1000000;
+        long myLong = 9876543210987L;
+        float myFloat = 19.99f;
+        double myDouble = 123.456789;
+        boolean myBoolean = false;
+        char myChar = 'Z';
+
+        System.out.println("--- Primitive Data Types ---");
+        System.out.println("byte: " + myByte);
+        System.out.println("short: " + myShort);
+        System.out.println("int: " + myInt);
+        System.out.println("long: " + myLong);
+        System.out.println("float: " + myFloat);
+        System.out.println("double: " + myDouble);
+        System.out.println("boolean: " + myBoolean);
+        System.out.println("char: " + myChar);
+
+
+	}
+
+}
