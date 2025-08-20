a2bb5b75260797456ee16c7ad4f9a01c24c43e50
Bhagya laxmi <Bhagya laxmi@Bhagya14>
2025-08-20 21:31:05 +0530

    LogicaloperatorDemo

---
+++ tns/src/LogicaloperatorDemo.java
@@ -0,0 +1,12 @@
+
+public class LogicaloperatorDemo {
+
+	public static void main(String[] args) {
+		boolean condition1 = true;
+		boolean condition2 = false;
+		System.out.println(condition1 && condition2); // Output: false
+		System.out.println(!condition1);             // Output: false
+
+	}
+
+}
