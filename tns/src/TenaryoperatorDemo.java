92557203860df3aaf536b67d45e3005cb1467a1a
Bhagya laxmi <Bhagya laxmi@Bhagya14>
2025-08-20 21:36:46 +0530

    TenaryoperatorDemo

---
+++ tns/src/TenaryoperatorDemo.java
@@ -0,0 +1,11 @@
+
+public class TenaryoperatorDemo {
+
+	public static void main(String[] args) {
+		int age = 20;
+		String status = (age >= 18) ? "Adult" : "Minor";
+		System.out.println(status); // Output: Adult
+
+	}
+
+}
