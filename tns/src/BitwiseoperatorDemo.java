438c92bee2bb8e693c602a2194b1c0f880c7f218
Bhagya laxmi <Bhagya laxmi@Bhagya14>
2025-08-20 21:35:04 +0530

    BitwiseoperatorDemo

---
+++ tns/src/BitwiseoperatorDemo.java
@@ -0,0 +1,11 @@
+
+public class BitwiseoperatorDemo {
+
+	public static void main(String[] args) {
+		int num1 = 5; // 0101 in binary
+		int num2 = 3; // 0011 in binary
+		System.out.println(num1 & num2); // Output: 1 (0001 in binary)
+
+	}
+
+}
