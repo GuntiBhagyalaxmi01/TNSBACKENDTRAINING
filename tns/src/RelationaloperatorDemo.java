4476c6f54d773751c701eed090aee6711b97cf33
Bhagya laxmi <Bhagya laxmi@Bhagya14>
2025-08-20 21:28:59 +0530

    RelationaloperatorDemo

---
+++ tns/src/RelationaloperatorDemo.java
@@ -0,0 +1,12 @@
+
+public class RelationaloperatorDemo {
+
+	public static void main(String[] args) {
+		int p = 8;
+		int q = 5;
+		System.out.println(p == q); // Output: false
+		System.out.println(p > q);  // Output: true
+
+	}
+
+}
